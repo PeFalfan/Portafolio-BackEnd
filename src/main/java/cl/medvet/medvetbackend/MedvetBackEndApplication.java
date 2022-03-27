7dcf65b73acf6f18f887a2c0b2d3a5afabb2a4b6
Pedro Falfan <pedro.falfan@hfsolutions.cl>
2022-03-26 22:56:59 -0300

    first commit

---
+++ src/main/java/cl/medvet/medvetbackend/MedvetBackEndApplication.java
@@ -0,0 +1,13 @@
+package cl.medvet.medvetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedvetBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedvetBackEndApplication.class, args);
+    }
+
+}
